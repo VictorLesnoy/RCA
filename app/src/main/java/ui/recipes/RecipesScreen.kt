846122de32460316e9519f24db2e9119ae22fa14
VictorLesnoy <martynovv@list.ru>
2026-06-06 16:49:03 +0500

    RCA-15-List_of_recipes

--- app/src/main/java/ui/recipes/RecipesScreen.kt
+++ app/src/main/java/ui/recipes/RecipesScreen.kt
@@ -9,6 +9,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import com.example.myapplication1.data.repository.RecipesRepositoryStub
 import com.example.myapplication1.ui.components.ScreenHeader
 import com.example.myapplication1.ui.theme.Dimens
