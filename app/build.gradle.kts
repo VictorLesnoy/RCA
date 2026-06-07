064f4e547a086ddcce4672ce625528f514dc5c75
VictorLesnoy <martynovv@list.ru>
2026-06-07 23:24:33 +0500

    RCA-15-List_of_recipes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,8 @@
 plugins {
+    //id("com.android.application")
+    //id("org.jetbrains.kotlin.android")
+    //kotlin("plugin.compose") version "1.9.23" // <-- обязательно
+    id("androidx.compose.compiler")
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
@@ -34,11 +38,12 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"  // Явно указываем версию компилятора Compose
+        kotlinCompilerExtensionVersion = "1.7.0"  // Явно указываем версию компилятора Compose
     }
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -50,10 +55,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
+    /*androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(libs.coil.compose)
-    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.navigation:navigation-compose:2.7.5")*/
 }
--- app/src/main/java/com/example/myapplication1/ui/recipes/RecipeItem.kt
+++ app/src/main/java/com/example/myapplication1/ui/recipes/RecipeItem.kt
@@ -9,7 +9,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import androidx.compose.ui.graphics.Color
+import coil.compose.SubcomposeAsyncImage
 import com.example.myapplication1.ui.theme.Dimens
 
 @Composable
