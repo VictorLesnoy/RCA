63ff1ec9fa19b33fe832317e573c3d07ede6f237
VictorLesnoy <martynovv@list.ru>
2026-07-25 21:11:47 +0500

    RCA-20-State_management_in_Compose

--- app/src/main/java/com/example/myapplication1/ui/components/ScreenHeader.kt
+++ app/src/main/java/com/example/myapplication1/ui/components/ScreenHeader.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import coil.compose.ContentScale
+import androidx.compose.ui.layout.ContentScale
 
 import com.example.myapplication1.R
 import com.example.myapplication1.ui.theme.Dimens
